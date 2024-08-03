104fed12251622c64a0260a55dfdfb6516b4b2cd
taigo2023 <taigo0617@gmail.com>
2024-08-03 17:02:25 +0900

    ルーティング　user admin 設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
+  devise_for :users, controllers: {
+    sessions: 'user/sessions',
+    registrations: 'user/registrations'
+  }
 end
