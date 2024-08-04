11eb6a2c0226013b621cd5c3c82db0b79288112e
taigo2023 <taigo0617@gmail.com>
2024-08-04 18:50:33 +0900

    商品（管理）　ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
     registrations: 'user/registrations'
   }
   root 'home#index'
+
+  namespace :admin do
+    resources :products, only: %i[index show new create edit update]
+  end
+ 
+  get '/up/', to: 'up#index', as: :up
+  get '/up/databases', to: 'up#databases', as: :up_databases
 end
