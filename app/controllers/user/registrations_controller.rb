798aaaa70b16bd06f5a6dfb48ebe595863337f11
taigo2023 <taigo0617@gmail.com>
2024-08-05 19:58:53 +0900

    その他

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -47,7 +47,8 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: [:name, :email, :password, :password_confirmation, :current_password])
   end
 
   # The path used after sign up.
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-    with_options presence: true do
+  with_options presence: true do
     validates :name
     validates :description
     validates :price
