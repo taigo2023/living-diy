8e0fca12e2d05e07682aca4ed13ea5b1c7b8c249
taigo2023 <taigo0617@gmail.com>
2024-08-05 19:56:27 +0900

    アプリケーションコントローラー作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, unless: :admin_namespace?
+
+  private
+
+  def authenticate_admin!
+    redirect_to new_admin_session_path unless admin_signed_in?
+  end
+
+  def admin_signed_in?
+    current_admin.present?
+  end
+
+  def admin_namespace?
+    controller_path.split('/').first == 'admin'
+  end
 end
